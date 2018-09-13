e58df65d7db88ce6acdf1a8288df66f977c651a2
Eric Rodriguez <eric.rodriguez@eniro.com>
2018-09-13 23:17:05 +0200

    Start test implementations

--- src/test/scala/scalaz/metrics/DropwizardMetricsSpec.scala
+++ src/test/scala/scalaz/metrics/DropwizardMetricsSpec.scala
@@ -10,38 +10,34 @@ object DropwizardMetricsSpec extends App {
 
   val f = IO.point(println("Hola"))
 
-  def increaseCounter: IO[IOException, Unit] = for {
+  def performTests: IO[IOException, Unit] = for {
     f <- dropwizardMetrics.counter(Label(Array("test", "counter")))
-    a <- f(1)
-    b <- f(2)
-  } yield b
+    _ <- f(1)
+    _ <- f(2)
+    _ <- dropwizardMetrics.gauge(Label(Array("test", "gauge")))(IO.point(5L))
+  } yield ()
 
-  def measureGauge: IO[IOException, Unit] = for {
-    a <- dropwizardMetrics.gauge(Label(Array("test", "gauge")))(IO.point(5L))
-  } yield a
+  def run(args: List[String]): IO[Nothing, ExitStatus] =
+    performTests
+    .attempt
+    .map(ei => {
+      printMetrics
+      ei.fold(_ => 1, _ => 0)
+    })
+    .map(ExitStatus.ExitNow(_))
 
-  def run(args: List[String]): IO[Nothing, ExitStatus] = {
-    increaseCounter.attempt
-      .map(ei => {
-        println(
-          dropwizardMetrics.registry
-            .getCounters()
-            .get("test.counter")
-            .getCount()
-        )
-        ei.fold(_ => 1, _ => 0)
-      })
-
-    measureGauge.attempt
-      .map(ei => {
-        println(
-          dropwizardMetrics.registry
-            .getGauges()
-            .get("test.gauge")
-            .getValue
-        )
-        ei.fold(_ => 1, _ => 0)
-      })
-      .map(ExitStatus.ExitNow(_))
+  def printMetrics() = {
+    println(
+      dropwizardMetrics.registry
+        .getCounters()
+        .get("test.counter")
+        .getCount()
+    )
+    println(
+      dropwizardMetrics.registry
+        .getGauges()
+        .get("test.gauge")
+        .getValue()
+    )
   }
 }
